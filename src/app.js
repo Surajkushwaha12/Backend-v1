506a108319e4aa3a44f47b68f88227034c87ec4c
TaskManagement <surajkushwaha8272@gmail.com>
2024-08-03 17:21:52 +0530

    All setup configure

--- src/app.js
+++ src/app.js
@@ -25,6 +25,13 @@ app.use(
 );
 
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)
 
 export { app };
