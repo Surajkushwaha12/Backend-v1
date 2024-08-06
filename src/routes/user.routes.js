6d4264dde12f8a00787c870baddb1feb25c4543c
TaskManagement <surajkushwaha8272@gmail.com>
2024-08-06 15:33:02 +0530

    Add Refresh end point token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
@@ -18,12 +23,12 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
 
-router.route("/logout").post(
-  verifyJWT,
-  logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
